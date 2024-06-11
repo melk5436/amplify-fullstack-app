8294d52bf14e75d762acd61dad10190f33f1d755
Ahmad Hamdalla <ahmadhamdalla5@gmail.com>
2024-06-11 07:49:41 -0500

    testing1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("Edit src/App.js and save to reload.");
+
+  const changeMessage = () => {
+    setMessage("You clicked the button! Edit src/App.js again.");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
+        <button onClick={changeMessage} className="App-button">
+          Click me
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
